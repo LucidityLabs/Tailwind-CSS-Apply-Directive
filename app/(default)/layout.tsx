6677c46975a517f8e5d0b8de71f76a79812f9b38
Lucidity Labs <mail@luciditylabs.tech>
2023-04-14 17:29:58 +0000

    Update 10 files

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -1,28 +1,29 @@
-'use client'
+"use client";
 
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
-import AOS from 'aos'
-import 'aos/dist/aos.css'
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function DefaultLayout({
   children,
 }: {
-  children: React.ReactNode
-}) {  
-
+  children: React.ReactNode;
+}) {
   useEffect(() => {
     AOS.init({
       once: true,
-      disable: 'phone',
+      disable: "phone",
       duration: 700,
-      easing: 'ease-out-cubic',
-    })
-  })
+      easing: "ease-out-cubic",
+    });
+  });
 
   return (
     <>
+      <div className="flex min-h-screen items-center justify-center bg-gradient-to-tr from-[#6672a8] to-[#d696b3]">
         {children}
+      </div>
     </>
-  )
+  );
 }
--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,20 +1,5 @@
-"use client"
-import DarkModeExampleBox from "@/components/DarkModeExampleBox";
-import { useState, useEffect } from "react";
+"use client";
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const root = window.document.documentElement;
-    if (darkMode) root.classList.add("dark");
-    else root.classList.remove("dark");
-  }, [darkMode]);
-
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-4 bg-gradient-to-tr from-[#6672a8] to-[#d696b3]">
-      <DarkModeExampleBox darkMode={darkMode} setDarkMode={setDarkMode} />
-      {/* <DarkModeExampleBox darkMode={darkMode} setDarkMode={setDarkMode} /> */}
-    </div>
-  );
+  return <button className="btn">Custom Button</button>;
 }
